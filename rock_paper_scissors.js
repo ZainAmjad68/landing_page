c9c1cbb09a56ecb0bdff19935fa1f1fa93d25c4e
ZainAmjad68 <zainamjad68@yahoo.com>
2021-09-28 15:27:49 +0500

    rock_paper_scissors implemented

---
+++ rock_paper_scissors.js
@@ -0,0 +1,49 @@
+function play() {
+  let i = 0;
+  while (i < 5) {
+    let playerTurn = window.prompt("Enter Rock, Paper or Scissor:");
+    makeATurn(playerTurn);
+  }
+}
+
+function makeATurn(playerTurn) {
+  let possible = ["rock", "paper", "scissor"];
+  let item = possible[Math.floor(Math.random() * possible.length)];
+
+  if (playerTurn === "rock") {
+    playerTurn = 0;
+  } else if (playerTurn === "paper") {
+    playerTurn = 1;
+  } else if (playerTurn === "scissor") {
+    playerTurn = 2;
+  }
+
+  if (item === "rock") {
+    item = 0;
+  } else if (item === "paper") {
+    item = 1;
+  } else if (item === "scissor") {
+    item = 2;
+  }
+
+  var resultMap = Array.from(Array(3), () => new Array(3));
+  resultMap[0][0] = 0;
+  resultMap[0][1] = 1;
+  resultMap[0][2] = 2;
+  resultMap[1][0] = 2;
+  resultMap[1][1] = 0;
+  resultMap[1][2] = 1;
+  resultMap[2][0] = 1;
+  resultMap[2][1] = 2;
+  resultMap[2][2] = 0;
+
+  let result = resultMap[item][playerTurn]
+    ? 1
+    : "Player"
+    ? 2
+    : "Computer"
+    ? 0
+    : "Tie";
+
+  console.log("The Winner is: ", result);
+}
